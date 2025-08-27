37355fbe815c1ee483417e01943407029d31cf4f
Matti Tahvonen <matti@vaadin.com>
2025-08-27 10:28:07 +0300

    Source code link to GH

--- demo-app/src/main/java/org/vaadin/example/Layout.java
+++ demo-app/src/main/java/org/vaadin/example/Layout.java
@@ -1,5 +1,9 @@
 package org.vaadin.example;
 
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Anchor;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.dom.Style;
 import org.vaadin.firitin.appframework.MainLayout;
 
 public class Layout extends MainLayout {
@@ -7,4 +11,23 @@ public class Layout extends MainLayout {
     protected String getDrawerHeader() {
         return "}> Java in Browser";
     }
+
+    Anchor viewSource = new Anchor("", "View source");
+
+    private static final String baseSourceUrl = "https://github.com/mstahv/java-in-browser-examples/blob/main/src/main/java/%s.java";
+
+    @Override
+    public void setContent(Component content) {
+        super.setContent(content);
+        String name = content.getClass().getName();
+        viewSource.setHref(baseSourceUrl.formatted(name.replace(".", "/")));
+        if(!viewSource.isAttached()) {
+            HorizontalLayout links = new HorizontalLayout(viewSource);
+            links.getStyle().setPosition(Style.Position.ABSOLUTE);
+            links.getStyle().setRight("1em");
+            links.getStyle().setTop("1em");
+            addToNavbar(true, links);
+        }
+    }
+
 }
